05ee5556c1ff5b86b17aa029119ba2f316d43a6b
Matt Lee <matt@reddit.com>
2017-04-22 22:05:48 -0400

    Click to center camera on room

--- unity/Assets/Scripts/CameraController.cs
+++ unity/Assets/Scripts/CameraController.cs
@@ -1,6 +1,7 @@
 ﻿using UnityEngine;
 
 public class CameraController : MonoBehaviour {
+	const float lerpDuration = 0.3f;
 	const float rotateEasing = 0.25f;
 	public float speed;
 	
@@ -9,23 +10,43 @@ public class CameraController : MonoBehaviour {
 	float targetRotate = 0;
 	float currentRotate = 0;
 
+	bool lerping = false;
+	float lerpStartTime = 0f;
+	float lerpUntilTime = -1f;
+	Vector3 lerpStartPosition;
+	Vector3 lerpTargetPosition;
+	Quaternion lerpStartRotation;
+	Quaternion lerpTargetRotation;
+
 	// Update is called once per frame
 	void Update () {
+		if (lerping) {
+			if (lerpUntilTime >= Time.time) {
+				float deltaTime = (Time.time - lerpStartTime) / lerpDuration;
+				transform.position = Vector3.Lerp(lerpStartPosition, lerpTargetPosition, deltaTime);
+				transform.rotation = Quaternion.Lerp(lerpStartRotation, lerpTargetRotation, deltaTime);
+			} else {
+				lerping = false;
+				transform.position = lerpTargetPosition;
+				transform.rotation = lerpTargetRotation;
+			}
+		}
+
 		Vector3 movement = new Vector3(
 			Input.GetAxis("Horizontal"),
 			Input.GetAxis("Vertical"),
 			Input.GetAxis("Zoom")
 		);
-		transform.Translate(movement * speed * Time.deltaTime);
+		
+		transform.position += transform.rotation * movement * speed * Time.deltaTime;
+		// transform.Translate(movement * speed * Time.deltaTime);
 
 		int currentRotateBtnDown = (int) Input.GetAxisRaw("Rotate");			
 		if (prevRotateBtnDown == 0) {
 			if (currentRotateBtnDown > 0) {
 				targetRotate -= 90f;
-				// transform.RotateAround(Vector3.zero, Vector3.up, -90f);
 			} else if (currentRotateBtnDown < 0) {
 				targetRotate += 90f;
-				// transform.RotateAround(Vector3.zero, Vector3.up, 90f);
 			}
 		}
 		prevRotateBtnDown = currentRotateBtnDown;
@@ -41,6 +62,26 @@ public class CameraController : MonoBehaviour {
 				currentRotate += deltaRotate;
 			}
 		}
+	}
+
+	public void FocusCameraOnPoint(Transform target, int rotate) {
+		Vector3 cameraPositionUnrotated = Quaternion.Inverse(transform.rotation) * transform.position;
+		Vector3 targetPositionUnrotated = Quaternion.Inverse(target.rotation) * target.position;
+
+		float deltaZ = cameraPositionUnrotated.z - targetPositionUnrotated.z;
+		Vector3 newPositionUnrotated = new Vector3(
+			targetPositionUnrotated.x,
+			targetPositionUnrotated.y,
+			targetPositionUnrotated.z + deltaZ
+		);
+
+		lerping = true;
+		lerpStartTime = Time.time;
+		lerpUntilTime = Time.time + lerpDuration;
+		lerpStartPosition = transform.position;
+		lerpTargetPosition = target.rotation * newPositionUnrotated;
+		lerpStartRotation = transform.rotation;
+		lerpTargetRotation = target.rotation;
 	}
 
 }
